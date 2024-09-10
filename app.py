42f535988f64526eddccbe707879ed874f011103
juanca164 <100615152+juanca164@users.noreply.github.com>
2024-09-10 00:38:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ def calculo_hora_extra(sueldo_base, horas_extras, horas_trabajadas_semana, recar
 
 st.title("Cálculo de Horas Extras")
 
-sueldo_base = st.number_input("Ingrese el sueldo base mensual", min_value=0.0)
-horas_extras = st.number_input("Ingrese cantidad de horas extras trabajadas", min_value=0.0)
-horas_trabajadas_semana = st.number_input("Ingrese cuántas horas trabaja a la semana", min_value=0.0)
+sueldo_base = st.number_input("Ingrese el sueldo base mensual", min_value=0)
+Choras_extras = st.number_input("Cantidad de horas extras trabajadas", min_value=0.0)
+horas_trabajadas_semana = st.number_input(" Cuántas horas trabaja a la semana", min_value=0)
 recargo_hora = st.number_input("Ingrese el recargo por hora extra (por ejemplo, 0.50 para 50%)", min_value=0.0)
 
 if st.button("Calcular"):
     valor_hora_extra = calculo_hora_extra(sueldo_base, horas_extras, horas_trabajadas_semana, recargo_hora)
-    st.success(f"El valor total por las horas extras trabajadas es: ${valor_hora_extra:.2f}")
+    st.success(f"El valor total por las horas extras trabajadas es: ${valor_hora_extra:,.2f}")
